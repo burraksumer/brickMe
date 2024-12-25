ff2e1e1475504fe35de027b1a2ff246b290cf1c8
Burak Sümer <buraksumer13@gmail.com>
2024-12-25 18:07:13 +0300

    fix checkout redirect url

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -8,25 +8,19 @@ class CheckoutController extends Controller
 {
     public function miniMaker(Request $request)
     {
-        return $request->user()->checkout(env('LEMON_SQUEEZY_MINI_VARIANT_ID'), [
-            'success_url' => route('dashboard'),
-            'cancel_url' => route('dashboard'),
-        ]);
+        return $request->user()->checkout(env('LEMON_SQUEEZY_MINI_VARIANT_ID'))
+            ->redirectTo(route('dashboard'));
     }
 
     public function starter(Request $request)
     {
-        return $request->user()->checkout(env('LEMON_SQUEEZY_STARTER_VARIANT_ID'), [
-            'success_url' => route('dashboard'),
-            'cancel_url' => route('dashboard'),
-        ]);
+        return $request->user()->checkout(env('LEMON_SQUEEZY_STARTER_VARIANT_ID'))
+            ->redirectTo(route('dashboard'));
     }
 
     public function pro(Request $request)
     {
-        return $request->user()->checkout(env('LEMON_SQUEEZY_PRO_VARIANT_ID'), [
-            'success_url' => route('dashboard'),
-            'cancel_url' => route('dashboard'),
-        ]);
+        return $request->user()->checkout(env('LEMON_SQUEEZY_PRO_VARIANT_ID'))
+            ->redirectTo(route('dashboard'));
     }
 }
