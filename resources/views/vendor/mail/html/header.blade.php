b077b359228032b693a782d5c1a553f4b1f7aa93
Burak Sümer <buraksumer13@gmail.com>
2024-12-26 17:47:59 +0300

    use embed for displaying the message

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,12 @@
 @props(['url'])
 <tr>
     <td class="header">
-        <a href="{{ $url }}">
+        <a
+            href="{{ $url }}"
+            style="display: inline-block;"
+        >
             <img
-                src="data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMzE2IDMxNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCB3aWR0aD0iMzE2IiBoZWlnaHQ9IjMxNiIgZmlsbD0iI0ZGRDkwMCIvPjxyZWN0IHg9Ijk4IiB5PSIxNDUiIHdpZHRoPSIxMjAiIGhlaWdodD0iMjYiIGZpbGw9IiMxQTFBMUEiLz48L3N2Zz4="
+                src="{{ $message->embed(asset('img/logo.svg')) }}"
                 alt="{{ config('app.name') }}"
                 style="width: 100px; height: auto;"
             >
