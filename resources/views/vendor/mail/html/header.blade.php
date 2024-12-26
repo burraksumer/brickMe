53cac43de47b790adb07fe00bf32610bc22afced
Burak Sümer <buraksumer13@gmail.com>
2024-12-26 17:34:21 +0300

    change base64 to inline base64 for visibility

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,12 @@
 @props(['url'])
 <tr>
     <td class="header">
-        <a
-            href="{{ $url }}"
-            style="display: inline-block;"
-        >
-            <x-mail-logo class="w-[100px] h-auto" />
+        <a href="{{ $url }}">
+            <img
+                src="data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMzE2IDMxNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCB3aWR0aD0iMzE2IiBoZWlnaHQ9IjMxNiIgZmlsbD0iI0ZGRDkwMCIvPjxyZWN0IHg9Ijk4IiB5PSIxNDUiIHdpZHRoPSIxMjAiIGhlaWdodD0iMjYiIGZpbGw9IiMxQTFBMUEiLz48L3N2Zz4="
+                alt="{{ config('app.name') }}"
+                style="width: 100px; height: auto;"
+            >
         </a>
     </td>
 </tr>
